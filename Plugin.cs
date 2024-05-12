f552a8f05040841529c2c3256dfb57d00df1281f
Nympfonic <arys.steam@gmail.com>
2024-05-12 21:54:23 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace SPT_Client_Mod_Template
+{
+    [BepInPlugin("com.YourName.ModName", "Mod name", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin is loaded!");
+        }
+    }
+}
